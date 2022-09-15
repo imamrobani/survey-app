619e8e8444351baceb87ef1ef9b1284b6dc63b63
Imam Robani <imam.robani@sicepat.com>
2022-09-15 22:14:23 +0700

    fix: naming folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {Colors, Fonts} from './constant';
+import {Colors, Fonts} from './const';
 
 const App = () => {
   return (
