9590c50ccc3215c5d57664356382923cc6874971
Imam Robani <imamrob@gmail.com>
2022-09-22 12:17:18 +0700

    fix: remove rn bootsplash and rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   rules: {
-    'comma-dangle': ['warn', 'never']
+    'comma-dangle': ['warn', 'never'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': ['warn', 'never']
   }
 };
--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import {NavigationContainer} from '@react-navigation/native';
 import Router from './router';
 import {enableScreens} from 'react-native-screens';
 import {Colors} from './const';
-import RNBootSplash from 'react-native-bootsplash';
 import 'react-native-gesture-handler';
 
 enableScreens();
-RNBootSplash.hide(); // immediate
 
 const MainApp = () => {
   return (
